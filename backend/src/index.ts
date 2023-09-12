92dce2b70631798a0edbad3272e5939ef52c5f8f
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-09-12 09:53:16 +0200

    initial

---
+++ backend/src/index.ts
@@ -0,0 +1,54 @@
+import express from "express"
+import fs from "fs/promises"
+import type { Request, Response } from "express"
+import cors from "cors"
+import {z} from "zod"
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+const ImageSchema = z.object({
+    name: z.string(),
+    title: z.string(),
+    url: z.string(),
+    id: z.string(),
+})
+
+app.get('/api/images', async (req: Request, res: Response) => {
+    const data = await fs.readFile("./data/data.json", "utf-8")
+    res.send(JSON.parse(data)) 
+})
+
+app.post("/api/images", async (req: Request, res: Response) => {
+
+    const result = ImageSchema.safeParse(req.body)
+    if(!result.success) {
+        return res.sendStatus(400)
+    }
+
+    const imageData = await fs.readFile("./data/data.json", "utf-8")
+    const images = JSON.parse(imageData)
+    images.push(result.data)
+    await fs.writeFile("./data/data.json", JSON.stringify(images), "utf-8")
+    
+    res.status(200).json(images)
+})
+
+app.delete("/api/images/:id", async (req: Request, res: Response) => {
+    const id = req.params.id
+    
+    const imageData = await fs.readFile("./data/data.json", "utf-8")
+    let images = JSON.parse(imageData)
+    images = images.filter((image: { id: string }) => image.id !== id)
+    
+    await fs.writeFile("./data/data.json", JSON.stringify(images), "utf-8")
+
+    res.status(200).json(images)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ frontend/src/main.ts
@@ -0,0 +1,114 @@
+import "./style.css";
+import Swiper from "swiper/bundle";
+import axios from "axios";
+import { nanoid } from 'nanoid'
+
+const swiperWrapper = document.querySelector(".swiper-wrapper") as HTMLDivElement;
+const formDOM = document.querySelector("#form") as HTMLFormElement;
+const nameInputDOM = document.querySelector("#name") as HTMLInputElement;
+const titleInputDOM = document.querySelector("#title") as HTMLInputElement;
+const urlInputDOM = document.querySelector("#url") as HTMLInputElement;
+
+
+type ImagesData = {
+  name: string,
+  title: string,
+  url: string,
+  id: string,
+}
+
+const swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const getData = async () => {
+
+    const response = await axios.get("http://localhost:3000/api/images");
+    const result = response.data
+
+    renderImages(result)
+};
+
+const renderImages = (result: any) => {
+  for (let i = 0; i < result.length; i++) {
+
+    swiperWrapper.innerHTML += `
+    <div class="swiper-slide" >
+      <img class="images" id=${result[i].id} src="${result[i].url}" />
+    </div>
+  `;
+  }
+
+  const images = document.getElementsByClassName("images") as HTMLCollectionOf<HTMLButtonElement>;
+  for (let i = 0; i < images.length; i++) {
+    const element = images[i];
+    element.addEventListener("dblclick", event);
+  }
+};
+
+const event =  async (e: MouseEvent) => {
+  let id = ((e.target) as HTMLDivElement).id
+  try {
+    await axios.delete(`http://localhost:3000/api/images/${id}`)
+    .then((response) => {
+      console.log(response.data);
+      updateImages(response.data)
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const updateImages = (data: ImagesData) => {
+
+    swiperWrapper.innerHTML += `
+    <div class="swiper-slide">
+      <img id=${data.id} src="${data.url}" />
+    </div>
+  `;
+};
+
+formDOM.addEventListener("submit", async (e) => {
+  e.preventDefault()
+
+  const uploadJSON = async (imagesData: ImagesData) => {
+    try {
+      await axios
+        .post("http://localhost:3000/api/images", inputData)
+        updateImages(imagesData)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const inputData = {
+    name: nameInputDOM.value,
+    title: titleInputDOM.value,
+    url: urlInputDOM.value,
+    id: nanoid()
+  };
+
+  uploadJSON(inputData);
+});
+
+getData()
+
+
+
+
+
+
+
